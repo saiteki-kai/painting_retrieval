b80fbe83a5d93f16219d0f8c9f4266dce5c29c11
davidepietrasanta <davide.pietrasanta@virgilio.it>
2022-02-04 16:37:42 +0100

    ResNet50 updated, run with 42 classes. More: https://www.kaggle.com/davidepietrasanta/resnet50-train?scriptVersionId=86979160

--- scripts/preprocess_data_for_training.py
+++ scripts/preprocess_data_for_training.py
@@ -37,6 +37,48 @@ def dataframe_generator():
 #df = dataframe_generator() # To generate it
 df = pd.read_csv(data_folder + "not_all_data_info.csv") # If we have it already
 
+from collections import Counter
+
+c_train = Counter(df["genre"])
+c_test = Counter(df["genre"])
+
+list_other_genre = []
+for key, value in c_train.items():
+  if (value / len(df) * 100) < 1:
+    print(f"{key:25s}\t{(value / len(df) * 100):.3f}% \t{(c_test[key]/len(df) * 100):.3f}% ")
+    list_other_genre.append(key)
+
+"""
+  Many pictorial genres appear less than 1% of the time in the train set.
+  We decided to merge them into a single genre called 'other'.
+
+  GENRE                     TRAIN   TEST
+  bird-and-flower painting 	0.117% 	0.117% 
+  history painting         	0.862% 	0.862% 
+  literary painting        	0.547% 	0.547% 
+  interior                 	0.657% 	0.657% 
+  poster                   	0.280% 	0.280% 
+  advertisement            	0.080% 	0.080% 
+  panorama                 	0.019% 	0.019% 
+  cloudscape               	0.204% 	0.204% 
+  quadratura               	0.022% 	0.022% 
+  caricature               	0.226% 	0.226% 
+  capriccio                	0.231% 	0.231% 
+  veduta                   	0.228% 	0.228% 
+  battle painting          	0.351% 	0.351% 
+  calligraphy              	0.157% 	0.157% 
+  vanitas                  	0.031% 	0.031% 
+  pastorale                	0.123% 	0.123% 
+  wildlife painting        	0.321% 	0.321% 
+  miniature                	0.116% 	0.116% 
+  yakusha-e                	0.090% 	0.090% 
+  tessellation             	0.182% 	0.182% 
+  shan shui                	0.032% 	0.032% 
+  bijinga                  	0.093% 	0.093% 
+  urushi-e                 	0.001% 	0.001% 
+"""
+
+
 def get_image_index(filename):
   for index in range(df.shape[0]): #n_row
     if( df["filename"][index] == filename ):
@@ -79,6 +121,8 @@ def separate_image_in_folders(folder, save_folder_name):
       index = get_image_index(filename)
       if index != -1:
         genre = df["genre"][index]
+        if genre in list_other_genre:
+          genre = 'other'
         temp_path = os.path.join(save_folder, genre)
         if not os.path.exists( temp_path ):
           os.makedirs( temp_path )
@@ -94,4 +138,8 @@ def separate_image_in_folders(folder, save_folder_name):
   De-comment before run.
 """
 #separate_image_in_folders( train_folder, "refactored_train") 
-#separate_image_in_folders( test_folder, "refactored_test") 
+separate_image_in_folders( test_folder, "refactored_test") 
+
+
+print(f"There are {len(list_other_genre)} pictorial genres that appear less than 1%.")
+print(f"Now you just have to consider {42-len(list_other_genre)+1} classes!")

4324216a9c45dd1f4d248a848ac6914a52d0b1c8
saiteki-kai <giuseppe.magazu@gmail.com>
2022-02-10 02:35:41 +0100

    save evaluation results

--- scripts/evalutation.py
+++ scripts/evalutation.py
@@ -1,8 +1,10 @@
+import os
 import time
 import numpy as np
 import pandas as pd
+import json
 
-from src.config import DATASET_FOLDER, STANDARD_FEATURES_SIZE
+from src.config import DATASET_FOLDER, STANDARD_FEATURES_SIZE, OUTPUT_FOLDER
 from src.painting.dataset import Dataset
 from src.painting.retrieval import ImageRetrieval
 
@@ -10,10 +12,10 @@ if __name__ == "__main__":
 
     FEATURE = "resnet50"
     SIMILARITY = "euclidean"
-    RESULTS = 5
+    RESULTS = 10
 
-    dataset = Dataset(DATASET_FOLDER, image_size=(224, 224) if FEATURE == "resnet50" else STANDARD_FEATURES_SIZE,
-                      test_only=True)
+    image_size = (224, 224) if FEATURE == "resnet50" else STANDARD_FEATURES_SIZE
+    dataset = Dataset(DATASET_FOLDER, image_size=image_size, test_only=True)
 
     ir = ImageRetrieval(FEATURE, dataset, evaluation=True)
     ir.index(SIMILARITY)
@@ -23,13 +25,15 @@ if __name__ == "__main__":
     relevant_ids = [dataset.get_relevant_indexes(query_id) for query_id in query_ids]
 
     relevant_ids = pd.DataFrame({"genre": query_genres, "query_id": query_ids, "docs_ids": relevant_ids})
+    relevant_ids = relevant_ids.groupby("genre").sample(200, random_state=1234)
 
     metrics = ["precision_at_k", "recall_at_k", "average_precision"]
 
-    all_results = {}
+    genre_results = {}
+    avg_results = {m: [] for m in metrics}
 
     for i, (genre, row) in enumerate(relevant_ids.groupby('genre')):
-        print(f"[{i+1:2d}/20] {genre} ({len(row)} paintings)")
+        print(f"[{i + 1:2d}/20] {genre} ({len(row)} paintings)")
         q_ids = list(row['query_id'])
         d_ids = list(row['docs_ids'])
 
@@ -43,15 +47,21 @@ if __name__ == "__main__":
         )
         print(time.time() - start)
 
-        avg_results = {m: [] for m in metrics}
+        avg_genre_results = {m: [] for m in metrics}
 
         for res in results:
             for m in metrics:
+                avg_genre_results[m].append(res[m])
                 avg_results[m].append(res[m])
 
-        avg_results = {key: np.average(avg_results[key]) for key in avg_results.keys()}
-        print(avg_results)
+        avg_genre_results = {key: np.average(avg_genre_results[key]) for key in avg_genre_results.keys()}
+        print(avg_genre_results)
 
-        all_results[genre] = avg_results
+        genre_results[genre] = avg_genre_results
 
-    print(all_results)
+    avg_results = {key: np.average(avg_results[key]) for key in avg_results.keys()}
+    print(avg_results)
+
+    res = {"config": {"feature": FEATURE, "similarity": SIMILARITY, "n_results": RESULTS}, "avg": avg_results,
+           "by_genre": genre_results}
+    json.dump(res, open(os.path.join(OUTPUT_FOLDER, "results", f"{FEATURE}_{SIMILARITY}_@{RESULTS}.json"), "w"))

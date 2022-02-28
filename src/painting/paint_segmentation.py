1353f46f2544488764d304a92e671255cc699a35
lordfighterx <lordfighterx@gmail.com>
2022-02-28 14:46:42 +0100

    update hough based pipeline

--- src/painting/paint_segmentation.py
+++ src/painting/paint_segmentation.py
@@ -1,88 +1,28 @@
 import os
-
 import cv2
 import numpy as np
-import segmentation_models as sm
+import matplotlib.pyplot as plt
 import skimage.exposure as exposure
 from medpy.filter.smoothing import anisotropic_diffusion
 from skimage.morphology import convex_hull_image
+from utils import resize_with_max_ratio
+
+from src.config import MODEL_FOLDER, OUTPUT_FOLDER
+
+from tensorflow import keras
 from tensorflow.keras.preprocessing.image import img_to_array
+import segmentation_models as sm
 
-from src.config import OUTPUT_FOLDER
-from src.painting.utils import resize_with_max_ratio
-
-sm.set_framework('tf.keras')
 BACKBONE = 'resnet34'
 preprocess_input = sm.get_preprocessing('resnet34')
 
 
-def sobel(gray, ksize):
-    # apply sobel derivatives
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=ksize)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=ksize)
-
-    # optionally normalize to range 0 to 255 for proper display
-    sobelx_norm = exposure.rescale_intensity(sobelx, in_range='image', out_range=(0, 255)) \
-        .clip(0, 255).astype(np.uint8)
-    sobely_norm = exposure.rescale_intensity(sobelx, in_range='image', out_range=(0, 255)) \
-        .clip(0, 255).astype(np.uint8)
-
-    # square
-    sobelx2 = cv2.multiply(sobelx, sobelx)
-    sobely2 = cv2.multiply(sobely, sobely)
-
-    # normalize to range 0 to 255 and clip negatives
-    sobelx2 = exposure.rescale_intensity(sobelx2, in_range='image', out_range=(0, 255)) \
-        .clip(0, 255).astype(np.uint8)
-    sobely2 = exposure.rescale_intensity(sobely2, in_range='image', out_range=(0, 255)) \
-        .clip(0, 255).astype(np.uint8)
-
-    # add together and take square root
-    sobel_magnitude = cv2.sqrt(sobelx2 + sobely2)
-    sobel_magnitude = exposure.rescale_intensity(sobel_magnitude, in_range='image', out_range=(0, 255)) \
-        .clip(0, 255).astype(np.uint8)
-
-    return sobelx2, sobely2, sobel_magnitude
-
-
-def sobel_operator(gray, ksize=3):
-    sobel_x = cv2.Sobel(gray, cv2.CV_32F, 1, 0, ksize=ksize)
-    out_x = np.clip(sobel_x, 0, 255).astype(np.uint8)
-    mean = np.mean(sobel_x)
-    out_x[out_x <= mean] = 0
-
-    sobel_y = cv2.Sobel(gray, cv2.CV_32F, 0, 1, ksize=ksize)
-    out_y = np.clip(sobel_y, 0, 255).astype(np.uint8)
-    mean = np.mean(sobel_y)
-    out_y[out_y <= mean] = 0
-
-    return out_x, out_y
-
-
-def fillhole(input_image):
-    '''
-    input gray binary image  get the filled image by floodfill method
-    Note: only holes surrounded in the connected regions will be filled.
-    :param input_image:
-    :return:
-    '''
-    im_flood_fill = input_image.copy()
-    h, w = input_image.shape[:2]
-    mask = np.zeros((h + 2, w + 2), np.uint8)
-    im_flood_fill = im_flood_fill.astype("uint8")
-    cv2.floodFill(im_flood_fill, mask, (0, 0), 255)
-    im_flood_fill_inv = cv2.bitwise_not(im_flood_fill)
-    img_out = input_image | im_flood_fill_inv
-    return img_out
-
-
 def false_colors(image, nb_components):
     # Create false color image
-    colors = np.random.randint(0, 255, size=(nb_components, 3), dtype=np.uint8)
+    colors = np.random.randint(0, 255, size=(nb_components , 3), dtype=np.uint8)
     colors[0] = [0, 0, 0]
-    # colors[0] = [207, 59, 0]
+    #colors[0] = [207, 59, 0]
     return colors[image]
-
 
 def hough_transform(image, rho=1, theta=np.pi / 180, threshold=30):
     '''
@@ -93,9 +33,8 @@ def hough_transform(image, rho=1, theta=np.pi / 180, threshold=30):
     '''
     return cv2.HoughLines(image, rho=rho, theta=theta, threshold=threshold)
 
-
 def draw_lines(image, lines, color=[255, 0, 0], thickness=2):
-    line_length = 100000  # impostare uguale alla diagonale
+    line_length=100000 #impostare uguale alla diagonale
     for line in lines:
         for rho, theta in line:
             t = theta * 180 / np.pi
@@ -116,7 +55,6 @@ def draw_lines(image, lines, color=[255, 0, 0], thickness=2):
             cv2.line(image, (x1, y1), (x2, y2), color, thickness)
     return image
 
-
 def corner_detection(image, T=0.01, min_dist=50):
     corners_points = []
     corners_image = np.zeros_like(image)
@@ -126,7 +64,7 @@ def corner_detection(image, T=0.01, min_dist=50):
     pts_copy = np.int0([pt[0] for pt in pts_copy])
 
     # compute the distance from each corner to every other corner
-    euclidian_dist = lambda pt1, pt2: np.sqrt((pt2[1] - pt1[1]) ** 2 + (pt2[0] - pt1[0]) ** 2)
+    euclidian_dist = lambda pt1, pt2 : np.sqrt((pt2[1] - pt1[1]) ** 2 + (pt2[0] - pt1[0]) ** 2)
 
     # if the points are not 4 we return zero points!
     if len(pts_copy) == 4:
@@ -143,27 +81,6 @@ def corner_detection(image, T=0.01, min_dist=50):
             cv2.circle(corners_image, tuple(pt), 10, 255, -1)
 
     return corners_points, corners_image
-
-
-def get_destination_points(corners):
-    w1 = np.sqrt((corners[0][0] - corners[1][0]) ** 2 + (corners[0][1] - corners[1][1]) ** 2)
-    w2 = np.sqrt((corners[2][0] - corners[3][0]) ** 2 + (corners[2][1] - corners[3][1]) ** 2)
-    w = max(int(w1), int(w2))
-
-    h1 = np.sqrt((corners[0][0] - corners[2][0]) ** 2 + (corners[0][1] - corners[2][1]) ** 2)
-    h2 = np.sqrt((corners[1][0] - corners[3][0]) ** 2 + (corners[1][1] - corners[3][1]) ** 2)
-    h = max(int(h1), int(h2))
-
-    destination_corners = np.float32([(0, 0), (0, w - 1), (h - 1, w - 1), (h - 1, 0)])
-
-    print('\nThe destination points are: \n')
-    for index, c in enumerate(destination_corners):
-        character = chr(65 + index) + "'"
-        print(character, ':', c)
-
-    print('\nThe approximated height and width of the original image is: \n', (h, w))
-    return destination_corners, h, w
-
 
 def warp_image(img, src):
     # invert points coordinates
@@ -192,35 +109,70 @@ def warp_image(img, src):
     return warp
 
 
-def hough_based_segmentation(image):
+def hough_based_segmentation(image, folder, save=False):
+    # resize
+    resized = resize_with_max_ratio(image, 1024, 1024)
+
     # grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
     # denoise
-    blur = anisotropic_diffusion(gray, kappa=30, gamma=0.25, option=1, niter=10).astype(np.uint8)  # parametri paper
+    blur = anisotropic_diffusion(gray, kappa=30, gamma=0.25, option=1, niter=10).astype(np.uint8) # parametri paper
 
     # edge detection
-    sx, sy = sobel_operator(blur)
+    sx = cv2.Sobel(blur, cv2.CV_32F, 1, 0, ksize=3)
+    sy = cv2.Sobel(blur, cv2.CV_32F, 0, 1, ksize=3)
 
-    # hough transform (tuning parametri di hough da fare)
-    h_lines = hough_transform(sx, threshold=200)
-    v_lines = hough_transform(sy, threshold=200)
+    sx = cv2.convertScaleAbs(sx)
+    sy = cv2.convertScaleAbs(sy)
 
-    out = draw_lines(gray, h_lines, color=255, thickness=3)
-    out = draw_lines(out, v_lines, color=255, thickness=3)
-    return out
+    #normalize / scale the image 0 to 255
+    out_x = cv2.normalize(sx, None, 0, 255, cv2.NORM_MINMAX)
+    out_y = cv2.normalize(sy, None, 0, 255, cv2.NORM_MINMAX)
 
+    #round up and type cast to int from float
+    out_x = np.round(out_x).astype(np.uint8)
+    out_y = np.round(out_y).astype(np.uint8)
 
-def cnn_based_segmentation(image, model, folder):
+    #use a threshold value
+    _, out_x = cv2.threshold(out_x, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    _, out_y = cv2.threshold(out_y, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    # horizontal hough transform
+    h_lines = hough_transform(out_x, threshold=210)
+    h_hough_out = draw_lines(np.zeros(sx.shape), h_lines, color=255, thickness=3)
+
+    # vertical hough transform
+    v_lines = hough_transform(out_y, threshold=210)
+    v_hough_out = draw_lines(np.zeros(sy.shape), v_lines, color=255, thickness=3)
+
+    # both horizontal and vertical hough
+    out = np.zeros(resized.shape)
+    out[:,:,0] = blur
+    out[:,:,1] = blur
+    out[:,:,2] = blur
+    hough_out = draw_lines(out, v_lines, color=[0,0,255], thickness=2)
+    hough_out = draw_lines(hough_out, h_lines, color=[0,0,255], thickness=2)
+
+    if save:
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '0_resized.jpg'), image)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '1_gray.jpg'), gray)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '2_blur.jpg'), blur)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '3_sobelx.jpg'), out_x)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '4_sobely.jpg'), out_y)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '5_h_hough.jpg'), h_hough_out*255)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '6_v_hough_out.jpg'), v_hough_out*255)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '7_hough_out.jpg'), hough_out)
+
+    return hough_out
+
+def cnn_based_segmentation(image, model, folder, save=False):
     x = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     x = cv2.resize(x, (224, 224))
     x = img_to_array(x) / 255.0
     x = preprocess_input(x)
     x = np.expand_dims(x, axis=0)
-
-    # with tf.device('/cpu:0'):
     pred = model.predict(x, batch_size=1)
-
     mask = (pred.squeeze() >= 0.5).astype(np.uint8)
     mask = cv2.resize(mask, (image.shape[1], image.shape[0]))
 
@@ -232,16 +184,18 @@ def cnn_based_segmentation(image, model, folder):
     # masked
     masked = cv2.bitwise_and(image, image, mask=max_bw_label)
 
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '1_rgb.jpg'), image)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '2_mask.png'), mask * 255)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '3_labeling_connected_components.png'),
-                false_colors(output, nb_components))
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '4_max_bw_label.png'), max_bw_label * 255)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '5_masked.png'), masked)
+    if save:
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '1_rgb.jpg'), image)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '2_mask.png'), mask * 255)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '3_labeling_connected_components.png'), false_colors(output, nb_components))
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '4_max_bw_label.png'), max_bw_label * 255)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '5_masked.png'), masked)
+
     return masked, max_bw_label
 
 
-def paint_segmentation_pipeline(image, folder, model=None):
+def paint_segmentation_pipeline(image, model=None, folder='.', save=False):
+
     # resize
     resized = resize_with_max_ratio(image, 1024, 1024)
 
@@ -254,73 +208,63 @@ def paint_segmentation_pipeline(image, folder, model=None):
     # morphology operations
     kernel = np.ones((3, 3), np.uint8)
     closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=30, borderValue=0)
-    fillholes = (fillhole(closing) > 0).astype(np.uint8)
-
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '6_close.png'), closing * 255)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '7_fillholes.png'), fillholes * 255)
 
     # convex hull
-    convex_hull = convex_hull_image(fillholes).astype(np.uint8)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '8_convex_hull.png'), convex_hull * 255)
+    convex_hull = convex_hull_image(closing).astype(np.uint8)
 
     # centroid of my mask edited with morphological operations and convexhull
-    x, y, w, h = cv2.boundingRect(convex_hull)
-    centroid = (x + round(w / 2), y + round(h / 2))
-
-    """show_centroid = cv2.circle(image, centroid, 15, (255, 0, 0), 5)
-    plt.imshow(show_centroid)
-    plt.show()"""
+    x,y,w,h = cv2.boundingRect(convex_hull)
+    centroid = (x+round(w/2), y+round(h/2))
 
     # edge detection
     (T, _) = cv2.threshold(convex_hull, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
     canny = cv2.Canny(convex_hull, T * 0.5, T)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '9_canny.png'), canny)
 
     # hough transform
     hough_lines = hough_transform(canny, threshold=30)
     hough_out = draw_lines(np.zeros(mask.shape), hough_lines, color=255, thickness=3)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '10_hough.png'), hough_out * 255)
 
     # invert hough mask
     invert_hough = 255 - hough_out
     invert_hough = (invert_hough > 0).astype(np.uint8)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '11_invert_hough.png'), invert_hough * 255)
 
     # labeling of the connected components [Note: range() starts from 1 since 0 is the background label.]
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(invert_hough, connectivity=4)
-    dist = lambda p1, p2: np.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+    dist = lambda p1, p2 : np.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
     min_label, min_dist = min([(i, dist(centroids[i], centroid)) for i in range(1, nb_components)], key=lambda x: x[1])
     min_dist_bw_label = (output == min_label).astype(np.uint8)
 
-    x1, x2, w1, h1 = cv2.boundingRect(min_dist_bw_label)
+    _,_,w1,h1 = cv2.boundingRect(min_dist_bw_label)
     min_dist = min(w1, h1) * 0.75
-
-    """copy = np.zeros(resized.shape)
-    copy[:,:,0] = min_dist_bw_label
-    copy[:,:,1] = min_dist_bw_label
-    copy[:,:,2] = min_dist_bw_label
-    cv2.rectangle(copy, (x1, x2), (x1+w1, x2+h1), (0, 255, 0), 3)"""
-
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '12_labeling_connected_components.png'),
-                false_colors(output, nb_components))
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '13_min_dist_bw_label.png'), min_dist_bw_label * 255)
 
     # corner detection
     corners_points, corners_image = corner_detection(min_dist_bw_label, T=0.01, min_dist=min_dist)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '14_harris.png'), corners_image)
 
-    if len(corners_points) != 4:
-        print(folder, len(corners_points))
+    if save:
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '6_close.png'), closing * 255)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '8_convex_hull.png'), convex_hull * 255)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '9_canny.png'), canny)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '10_hough.png'), hough_out * 255)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '11_invert_hough.png'), invert_hough * 255)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '12_labeling_connected_components.png'), false_colors(output, nb_components))
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '13_min_dist_bw_label.png'), min_dist_bw_label*255)
+        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, '14_harris.png'), corners_image)
 
     # if the corners detected are not 4 the warped image is not corrected!
     if len(corners_points) != 4:
-        x, y, w, h = cv2.boundingRect(min_dist_bw_label)
+        x,y,w,h = cv2.boundingRect(min_dist_bw_label)
         not_unwarped = cv2.bitwise_and(resized, resized, mask=min_dist_bw_label)
-        not_unwarped = not_unwarped[y:y + h, x:x + w]
-        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, 'not_un_warped.jpg'), not_unwarped)
+        not_unwarped = not_unwarped[y:y+h, x:x+w]
+
+        if save:
+            cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, 'not_un_warped.jpg'), not_unwarped)
+
         return not_unwarped, min_dist_bw_label
     else:
         # image distortion correction
         un_warped = warp_image(resized, corners_points)
-        cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, 'un_warped.jpg'), un_warped)
+        
+        if save:
+            cv2.imwrite(os.path.join(OUTPUT_FOLDER, folder, 'un_warped.jpg'), un_warped)
+
         return un_warped, min_dist_bw_label
